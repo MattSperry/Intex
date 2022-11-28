61e68d7dd79103b5dbe1aa764cc3e4b8aa00d012
MattSperry <matthewjoelsperry@gmail.com>
2022-11-28 16:14:08 -0700

    fixing migrations

--- INTEX/dashboard/models.py
+++ INTEX/dashboard/models.py
@@ -2,7 +2,6 @@ from django.db import models
  
 # Create your models here.
 class Comorbidity(models.Model):
-    comorbidityID = models.AutoField(primary_key=True, default=1)
     name = models.CharField(max_length=30)
 
     def __str__(self):
