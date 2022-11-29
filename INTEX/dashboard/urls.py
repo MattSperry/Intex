d923b743ca4acfe6982a1b5abcb5badc37163c6d
MattSperry <matthewjoelsperry@gmail.com>
2022-11-28 18:47:03 -0700

    login as default page

--- INTEX/dashboard/urls.py
+++ INTEX/dashboard/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
  
 urlpatterns = [
-    path("", indexPageView, name="dashboard-index"),
+    path("", loginPageView, name="dashboard-login"),
+    path("index/", indexPageView, name="dashboard-index"),
     path("register/", registerPageView, name="dashboard-register"),
     path("input/", inputPageView, name="dashboard-input"),
-    path("login/", loginPageView, name="dashboard-login"),
     path("logout", logoutPageView, name= "dashboard-logout"),
     path("journal/", journalPageView, name="dashboard-journal"),
     path("suggestions/", suggestionsPageView, name="dashboard-suggestions"),
